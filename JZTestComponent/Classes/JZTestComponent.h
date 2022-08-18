a51ffba1eff5965d1ec0967cc4223f919fe6cc2b
黄健志 <huangjz@galanz.com>
2022-08-18 14:20:57 +0800

    print hello world

--- JZTestComponent/Classes/JZTestComponent.h
+++ JZTestComponent/Classes/JZTestComponent.h
@@ -13,6 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)testPrint;
 
++ (void)printHelloWorld;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- JZTestComponent/Classes/JZTestComponent.m
+++ JZTestComponent/Classes/JZTestComponent.m
@@ -13,4 +13,8 @@
     NSLog(@"%s", __func__);
 }
 
++ (void)printHelloWorld{
+    NSLog(@"%s", __func__);
+}
+
 @end
